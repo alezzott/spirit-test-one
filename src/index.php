7e092b82845ff8568e43e198c800f45537bbd25f
Alex Faustino <faustino.alex@outlook.com>
2024-04-13 15:47:43 -0300

    fix: "search bar and card responsive"

--- src/index.php
+++ src/index.php
@@ -8,42 +8,44 @@
     <title>Listagem de Produtos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/home.css">
 </head>
 
 <body>
-
-    <!-- Barra de navegação -->
-    <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">Minha Loja</a>
+    <nav class="navbar navbar-dark bg-dark p-3">
+        <a class="navbar-brand" href="/">Alezzo shop</a>
     </nav>
 
     <div class="container mt-5">
-        <!-- Barra de pesquisa -->
-        <input type="text" id="searchInput" placeholder="Pesquisar por título..." onkeyup="productsFilter()">
+        <div class="input-group flex-nowrap">
+            <input type="text" id="searchInput" class="form-control" placeholder="Pesquisar por título..."
+                onkeyup="productsFilter()">
+        </div>
 
-        <h1 class="mt-3">Lista de Produtos</h1>
-        <div class="row mt-3" id="product-grid">
+
+
+
+
+        <h1 class="mt-3 text-product display-5">Lista de Produtos</h1>
+        <div class="row pt-3" id="product-grid">
             <?php
             foreach ($products as $product) {
-
                 if (isset($product['images']) && is_array($product['images']) && !empty($product['images'])) {
                     $firstImage = $product['images'][0];
                 } else {
                     $firstImage = '../assets/default-featured-image.png';
                 }
                 echo '
-                    <div class="col-sm-6 col-lg-4 mb-4">
-                        <div class="card">
-                        <div class="ratio ratio-4x3">
-                                <img src="' . $firstImage . '" class="card-img-top h-100" alt="' . $product['title'] . '">
-                            </div>
-                            <div class="card-body">
-                                <h5 class="card-title">' . $product['title'] . '</h5>
-                                <p class="card-text">' . $product['description'] . '</p>
-                                <a href="pages/product_details.php?id=' . $product['id'] . '" class="btn btn-primary">Ver Mais</a>
-                            </div>
+                <div class="col-sm-6 col-lg-4 mb-4">
+                    <div class="card h-100">
+                        <img src="' . $firstImage . '" class="card-img-top img-fluid p-3 rounded" style="height: 250px" alt="' . $product['title'] . '">
+                        <div class="card-body">
+                            <h5 class="card-title">' . $product['title'] . '</h5>
+                            <p class="card-text">' . $product['description'] . '</p>
+                            <a href="pages/product_details.php?id=' . $product['id'] . '" class="btn rounded btn-primary">Ver Mais</a>
                         </div>
                     </div>
+                </div>
                     ';
             }
             ?>
